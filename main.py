2e8bb787b41becb5eb17789d5cc9ea842df8991b
vicmaral <vicmaral87@gmail.com>
2024-11-06 16:42:04 +0100

    Works not perfect, but works

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-h_size = 800
-w_size = 1200
+h_size = 400
+w_size = 500
 
 
 def get_random_word():
     r = RandomWords()
     temp_word = r.get_random_word()
-    while len(temp_word) > 5:
+    while len(temp_word) > 8:
         temp_word = r.get_random_word()
     return temp_word
 
@@ -48,13 +48,13 @@ def key_pressed(event):
         output_word.config(text=typed_word)
         entry.delete(0, customtkinter.END)
         tk.Label(window, text="  Words:" + str(num_words), font=(FONT_NAME, 20, 'bold')
-                 ).grid(row=4, column=2)
+                 ).grid(row=4, column=3)
         tk.Label(window, text="  " + str(round((num_words / (time.time() - start) * 60), 2)) + "words/s  ",
                  font=(FONT_NAME, 20, 'bold')
-                 ).grid(row=4, column=1)
+                 ).grid(row=4, column=2)
         tk.Label(window, text=str(round((num_chars / (time.time() - start) * 60), 2)) + "chars/s  ",
                  font=(FONT_NAME, 20, 'bold')
-                 ).grid(row=4, column=0)
+                 ).grid(row=4, column=1)
     else:
 
         print('You pressed %s\n' % typed_word)
@@ -64,19 +64,20 @@ def key_pressed(event):
 #Create windows
 window = customtkinter.CTk()
 window.title("Typing Speed Tester")
-window.config(height=h_size, width=w_size, bg=YELLOW)
+window.geometry(str(w_size)+"x"+str(h_size))
+window.config(bg=YELLOW)
 
 # Draw Tittle
 title = customtkinter.CTkLabel(window, text="Typing Speed Tester", font=(FONT_NAME, 40, 'bold'), fg_color=YELLOW,
-                               ).grid(row=0, column=1, columnspan=2)
+                               ).grid(row=0, column=1, columnspan=3)
 word_to_type = tk.Label(window, text=random_word, font=(FONT_NAME, 20, 'bold'))
-word_to_type.grid(row=1, column=1, columnspan=2)
+word_to_type.grid(row=1, column=1, columnspan=3)
 
 entry = customtkinter.CTkEntry(window, height=2, width=100)
 entry.grid(row=2, column=1, columnspan=3)
 
 output_word = tk.Label(window, text=typed_word, font=(FONT_NAME, 20, 'bold'))
-output_word.grid(row=3, column=1, columnspan=2)
+output_word.grid(row=3, column=1, columnspan=3)
 
 window.bind('<KeyPress>', key_pressed)
 
